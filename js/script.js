df03e25ef87d049becd1c80e389362ebd3c9ec65
carloscMe <carloscarrillo.sprout@gmail.com>
2025-03-19 15:49:09 -0500

    repaso-1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const numero1 = prompt("ingresa el primer numero");
+/*const numero1 = prompt("ingresa el primer numero");  // en si funciona pero solo me toma el valor inicial y no la cifra 
 
 const numero2 = prompt("ingresa el segundo numero");
 
@@ -12,19 +12,32 @@ else if(numero2 > numero1){
 
 else{
     alert("tu numero es igual asi mismo");
-}
+}*/
 
 
 // con un metodo de array puedo recorrerlo y los numero que no aparezcan son impar en si tengo que repasar los metodos//
 
 /*const entrada = prompt("ingresa un numero");
 
-for(let i= 0; i < 1000; i += 2 ){
+let i;
+for( i= 0; i < 20; i += 2 ){
 
     if(entrada == i){
-        console.log(`el numero: ${entrada} es par. `);
+        alert(`el numero: ${entrada} es par. `);
     }
 }*/
+
+
+/*const edad = prompt("ingresa tu edad");
+
+if (edad >= 18 && edad <= 100) {
+    alert(`tienes ${edad} eres mayor de edad. `)
+}
+
+ else{
+    alert("no puedes pasar")
+ }*/
+
 
 
 
